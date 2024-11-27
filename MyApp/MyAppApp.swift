5340b0bd7ffebf4af197b47ef18f4d823484b375
hyliard <luisgerardomartinezh@gmail.com>
2024-11-26 22:28:15 -0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Hyliard on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
